a26738105782b6380d0f3e5b7e3bd80a5b9560af
Andre Hogenkamp <tikaltikal@gmail.com>
2017-05-05 22:55:27 +0200

    Basic vue.js html application

---
+++ app.js
@@ -0,0 +1,13 @@
+new Vue({
+  // We want to target the div with an id of 'events'
+  el: '#events'
+  // Here we can register any values or collections that hold data
+  // for the application
+  data: {},
+
+  // Anything within the ready function will run when the application loads
+  ready: function() {},
+
+  // Methods we want to use in our application are registered here
+  methods: {}
+});
